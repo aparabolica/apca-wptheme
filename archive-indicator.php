6ce5f3bb3dff688916bddfb575b2b76b3343942b
Miguel Peixe <miguel@cardume.art.br>
2016-08-05 12:46:28 -0300

    add indicator link to archive title

--- archive-indicator.php
+++ archive-indicator.php
@@ -13,7 +13,7 @@
       <article id="indicator-<?php the_ID(); ?>">
         <div class="container">
           <div class="eight columns offset-by-four">
-            <h2><?php the_title(); ?></h2>
+            <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
           </div>
         </div>
         <div class="container">
