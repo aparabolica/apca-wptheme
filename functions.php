3b87738d483f19b2535a37aec62bf92e7cc2c71b
Miguel Peixe <miguel@cardume.art.br>
2016-09-12 10:54:16 -0300

    update style version

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function apca_scripts() {
-  wp_register_style('apca-main', get_stylesheet_directory_uri() . '/css/main.css', array('main'), '0.0.1');
+  wp_register_style('apca-main', get_stylesheet_directory_uri() . '/css/main.css', array('main'), '0.0.2');
   wp_enqueue_style('apca-main');
 }
 add_action('wp_enqueue_scripts', 'apca_scripts');
