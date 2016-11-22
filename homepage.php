698d8e102ae4c94ff08813699ef579d2a23175e7
miguelpeixe <miguel@cardume.art.br>
2016-11-22 11:21:03 -0200

    add dynamic social media links to homepage

--- homepage.php
+++ homepage.php
@@ -76,8 +76,20 @@
         <div class="social-bg"></div>
         <div class="social-content">
           <h2><?php _e('Connect with us', 'apca'); ?></h2>
-          <a class="fa fa-facebook" href="https://www.facebook.com/WWFLivingAmazonInitiative/" target="_blank" rel="extenal" title="Facebook"></a>
-          <a class="fa fa-youtube" href="https://www.youtube.com/user/LivingAmazon" target="_blank" rel="external" title="YouTube"></a>
+          <?php
+          $fb = get_field('site_options_facebook', 'option');
+          $tw = get_field('site_options_twitter', 'option');
+          $yt = get_field('site_options_youtube', 'option');
+          ?>
+          <?php if($fb) : ?>
+            <a class="fa fa-facebook" href="<?php echo $fb; ?>" target="_blank" rel="extenal" title="Facebook"></a>
+          <?php endif; ?>
+          <?php if($yt) : ?>
+            <a class="fa fa-youtube" href="<?php echo $yt; ?>" target="_blank" rel="external" title="YouTube"></a>
+          <?php endif; ?>
+          <?php if($tw) : ?>
+            <a class="fa fa-twitter" href="<?php echo $tw; ?>" target="_blank" rel="external" title="Twitter"></a>
+          <?php endif; ?>
         </div>
       </section>
       <section id="latest" class="page-section clean">
